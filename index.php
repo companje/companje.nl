ffadf58d1656fe947f0ba2d92a7547a2d7b2f7b3
Rick Companje <rick@companje.nl>
2020-03-29 00:34:57 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,5 @@
 <?php
-echo "2";
-// ini_set('display_errors', 'On');
-// die("test");
-
-if (isset($_SERVER['HTTP_X_GITHUB_EVENT'])) {
-  // if ( $_POST['payload'] ) {
-  //if ($_SERVER['HTTP_X_GITHUB_EVENT'] == 'push') {
-  var_dump(shell_exec('git pull'));
-  die();
-}
+shell_exec('git pull');
 
 require __DIR__ . '/vendor/autoload.php';
 
