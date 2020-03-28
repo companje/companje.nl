7600e5452a4321df9e1c1184f44145f96f25e34d
Rick Companje <rick@companje.nl>
2020-03-28 22:08:27 +0100

    remove jekyll

--- scripts/backup-modified-dates.sh
+++
@@ -1,1 +0,0 @@
-find . -mount -print0 | perl -ne 'INIT{ $/ = "\0"; use File::stat;} chomp; my $s = stat($_); next unless $s; print $s->ctime . "/" . $s->mtime . "/" . $s->atime ."/$_\0"; ' > dates.dat
--- scripts/restore-modified.dates.sh
+++
@@ -1,2 +0,0 @@
-cat dates.dat |  perl -ne 'INIT{ $/ = "\0";} chomp; m!^([0-9]+)/([0-9]+)/([0-9]+)/(.*)!s or next; my ($ct, $mt, $at, $f) = ($1, $2, $3, $4); utime $at, $mt, $f;'
-# rm dates.dat
