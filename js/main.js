981e3e3e1cc96bbdfdbc8012995456436b799b0a
Rick Companje <rick@companje.nl>
2020-03-29 01:27:20 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,9 @@
 window.onkeydown = function(event) {
   if (event.keyCode==69 && event.metaKey) { //cmd+E
-    console.log('edit');
     var page = window.location.pathname;
     page = page.replace(new RegExp("/", "g"), "");
     page = page.replace(new RegExp(".html", "g"), "");
-    window.open("https://github.com/companje/companje.github.io/edit/master/" + page + ".md");
-    // var win = window.open("subl://"+page);
-    //window.open("subl://hoi")
-    // setTimeout(function() { win.close(); },1000);
+    window.open("https://github.com/companje/companje.github.io/edit/master/pages/" + page + ".md");
   }
 
   if (event.keyCode==191) { //  '/'
