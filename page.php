d657b2a0227b6246ac082b0fd25ac3b6e6091eb6
Rick Companje <rick@companje.nl>
2021-10-27 07:15:08 +0200

    Update page.php

--- page.php
+++ page.php
@@ -4,7 +4,7 @@ $pages = glob("pages/*.md");
 
 if (!in_array($url,$pages)) {
   //header("Location: https://github.com/companje/companje.github.io/new/master");
-  header("Location: https://github.com/companje/companje.nl/tree/master/pages");
+  //header("Location: https://github.com/companje/companje.nl/tree/master/pages");
   die();
 }
 
