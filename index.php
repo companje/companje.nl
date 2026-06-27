d3506b61ace6112d2dd41ae2dd72be057aa0b82c
Rick Companje <rick@companje.nl>
2026-06-27 22:04:36 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,16 +2,8 @@
 //ini_set('display_errors', 'On');
 //ini_set( "short_open_tag", 1 );
 
-
-if (isset($_SERVER['HTTP_X_GITHUB_EVENT']) && $_SERVER['HTTP_X_GITHUB_EVENT'] == 'push') {
-  shell_exec('git pull');
-  //file_put_contents("test.txt", "test2");
-  //die();
-}
-
 require __DIR__ . '/vendor/autoload.php';
 
-//$page = trim($_SERVER['REQUEST_URI'], '/');
 $page = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
 
 switch ($page) {
@@ -21,4 +13,3 @@ switch ($page) {
 }
 
 ?>
-
